2d9c23914ebeecc1deadc586225abcf3cbc1b01a
sujal3797 <sujalgupta3797@gmail.com>
2025-07-20 18:56:55 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import dotenv from "dotenv";
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error connecting to MongoDB");
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is running on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+})()
+*/
