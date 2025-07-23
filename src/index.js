71a3093243e66c97ad0dc99970b095c8370b781f
sujal3797 <sujalgupta3797@gmail.com>
2025-07-23 13:58:45 +0530

    setup completed

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 connectDB()
