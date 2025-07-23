a3fef5eb924213a9944f1020165ba41f242e51d4
sujal3797 <sujalgupta3797@gmail.com>
2025-07-23 17:33:22 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,7 +24,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required");
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
 
@@ -32,8 +32,15 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(409, "User with this username or email already exists");
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // console.log("req.files: ", req.files)
+
+    const avatarLocalPath = req.files?.avatar?.[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage?.[0]?.path
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath) {
         throw new ApiError(400, "Avatar is required");
@@ -59,13 +66,13 @@ const registerUser = asyncHandler(async (req, res) => {
         "-password -refreshToken"
     )
 
-    if(createdUser) {
+    if(!createdUser) {
         throw new ApiError(500, "Something went wrong while registering the user");
 
     }
 
     return res.status(201).json(
-        new ApiResponse(20, createdUser, "User registered successfully")
+        new ApiResponse(201, createdUser, "User registered successfully")
     )
 })
 
